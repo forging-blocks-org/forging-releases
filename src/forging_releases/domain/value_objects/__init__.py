e5869d2a85518c613f0c4a497426cc9df7effebf
Glauber Brennon <glauberbrennon@gmail.com>
2026-06-04 15:14:19 -0300

    feat(domain): add domain package exports

---
+++ src/forging_releases/domain/value_objects/__init__.py
@@ -0,0 +1,12 @@
+from .release_branch_name import ReleaseBranchName
+from .release_level import ReleaseLevel, ReleaseLevelEnum
+from .release_version import ReleaseVersion
+from .tag_name import TagName
+
+__all__ = [
+    "ReleaseVersion",
+    "ReleaseLevel",
+    "ReleaseLevelEnum",
+    "ReleaseBranchName",
+    "TagName",
+]

ebe2e8de68623a24b62ba1ed883d9c346d4f4b6e
Glauber Brennon <glauberbrennon@gmail.com>
2026-06-07 02:39:07 -0300

    feat: add SubprocessCommandRunner with integration tests

--- src/forging_releases/infrastructure/__init__.py
+++ src/forging_releases/infrastructure/__init__.py
@@ -1,23 +1,35 @@
-from forging_releases.infrastructure.git_changelog_generator import GitChangelogGenerator
-from forging_releases.infrastructure.git_version_control import GitVersionControl
-from forging_releases.infrastructure.github_pull_request_service import (
-    GitHubPullRequestService,
+from forging_releases.infrastructure.changelog_generator.changelog_generator import (
+    GitChangelogGenerator,
 )
-from forging_releases.infrastructure.in_memory_release_command_bus import (
+from forging_releases.infrastructure.command_bus.command_bus import (
     InMemoryReleaseCommandBus,
 )
-from forging_releases.infrastructure.in_memory_release_transaction import (
+from forging_releases.infrastructure.command_runner.command_runner import (
+    SubprocessCommandRunner,
+)
+from forging_releases.infrastructure.container import Container
+from forging_releases.infrastructure.handler.handler import OpenPullRequestHandler
+from forging_releases.infrastructure.pull_request_service.pull_request_service import (
+    GitHubPullRequestService,
+)
+from forging_releases.infrastructure.release_transaction.release_transaction import (
     InMemoryReleaseTransaction,
 )
-from forging_releases.infrastructure.pyproject_versioning_service import (
+from forging_releases.infrastructure.version_control.version_control import (
+    GitVersionControl,
+)
+from forging_releases.infrastructure.versioning_service.versioning_service import (
     PyProjectVersioningService,
 )
 
 __all__ = [
+    "Container",
     "GitChangelogGenerator",
     "GitHubPullRequestService",
     "GitVersionControl",
     "InMemoryReleaseCommandBus",
     "InMemoryReleaseTransaction",
+    "OpenPullRequestHandler",
     "PyProjectVersioningService",
+    "SubprocessCommandRunner",
 ]
---
+++ src/forging_releases/infrastructure/command_runner/__init__.py
@@ -0,0 +1,7 @@
+from forging_releases.infrastructure.command_runner.command_runner import (
+    SubprocessCommandRunner,
+)
+
+__all__ = [
+    "SubprocessCommandRunner",
+]

140d62e1ee818fe3dcaf0a6239903113840b1834
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-24 21:59:26 -0300

    feat(presentation): expose presentation layer interface

---
+++ src/forging_releases/presentation/__init__.py
@@ -0,0 +1,8 @@
+from forging_releases.infrastructure.container import Container
+
+from .presenters.release_cli_presenter import ReleaseCliPresenter
+
+__all__ = (
+    "Container",
+    "ReleaseCliPresenter",
+)

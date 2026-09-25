6904f47fbee5ba6d3d549cbe81de26b89434d1f5
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-24 21:59:45 -0300

    fix(errors): import Error from errors root in branch error

--- src/forging_releases/application/errors/release_branch_exists_error.py
+++ src/forging_releases/application/errors/release_branch_exists_error.py
@@ -1,7 +1,7 @@
 """Error raised when a release branch already exists with the same changes."""
 
+from forging_blocks.foundation.errors import Error
 from forging_blocks.foundation.errors.core import ErrorMessage
-from forging_blocks.foundation.errors.error import Error
 
 
 class ReleaseBranchExistsError(Error[dict[str, object]]):

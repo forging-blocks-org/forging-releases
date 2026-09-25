1d36be7931104d003e9219061f950bdd4c5ea386
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-24 21:59:57 -0300

    style(errors): clean up exports in application errors

--- src/forging_releases/application/errors/__init__.py
+++ src/forging_releases/application/errors/__init__.py
@@ -1,5 +1,4 @@
 from .change_log_generation_error import ChangelogGenerationError
-
 from .release_branch_exists_error import ReleaseBranchExistsError
 from .tag_already_exists_error import TagAlreadyExistsError
 

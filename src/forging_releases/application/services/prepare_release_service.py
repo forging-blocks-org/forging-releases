25cb4c52201eced8344b0b561c11bb036847b170
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-24 22:00:08 -0300

    fix(services): import Command from domain messages in prepare release service

--- src/forging_releases/application/services/prepare_release_service.py
+++ src/forging_releases/application/services/prepare_release_service.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from forging_blocks.foundation.messages.command import Command
+from forging_blocks.domain.messages.command import Command
 from forging_releases.application.ports.inbound import (
     PrepareReleaseInput,
     PrepareReleaseOutput,
